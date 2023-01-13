b3cf0f26048abb9e769fcbf3608fcdd4376f3e51
MishIvan <85023280+MishIvan@users.noreply.github.com>
2023-01-13 21:05:14 +0700

    Delete viewcreate.sql

--- TasksAccounting/texts/viewcreate.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW "WorksView"
-AS 
-select t2.TaskID, t1.Name, t1.Deadline, t1.FulfillmentDate,
-t2.ID WorkID, t2.Content, t2.Date, t2.TimeSpent 
-from tasks t1 left join works t2 on t1.ID = t2.TaskID
-order by t1.ID, t2.ID
